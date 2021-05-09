b71e63097557e909e49d5fa1644828e91cffc05a
Darsruiz <darsruiz@gmail.com>
2021-05-10 01:39:19 +0200

    added firebase

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Ejercicio Front-End en Angular
 
 Rover que se mueve cuando introduces coordenadas y se muestra en pantalla.
+
+https://rover-mars-3cbb0.web.app
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,25 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+// Firestore
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment.prod';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule
+    ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
